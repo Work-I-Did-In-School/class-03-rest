97f6d36b761065d4ac4fa76b9c252e1fed373fce
JacobKnaack <jacobknck@gmail.com>
2021-06-16 15:24:19 -0700

    updates environment config

--- src/models/index.js
+++ src/models/index.js
@@ -4,21 +4,22 @@ require('dotenv').config();
 // heroku uses this naming convention
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
 
+// We added this to our script, so that we can provide some info about our environement (Dev / test / production)
+const NODE_ENV = process.env.NODE_ENV;
+
 // Sequelize lets us create data models, DataType represent the Types of data we can create in SQL
 const { Sequelize, DataTypes} = require('sequelize');
 const foodModel = require('./food.js');
 
-
-
 // analogous to the express()
-let sequelize = new Sequelize(DATABASE_URL, {
+let sequelize = new Sequelize(DATABASE_URL, NODE_ENV === 'production' ? {
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false,
     }
   }
-});
+}: {});
 
 // modify the sequelize object using a model.
 const food = foodModel(sequelize,  DataTypes);
